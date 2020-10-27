a885061f164b404a2f608c80bb6bb6a5bcd64828
Prernasingh2902 <72907620+Prernasingh2902@users.noreply.github.com>
2020-10-27 13:27:39 +0530

    Add files via upload

--- Gneral-Insurance-Java/src/main/java/com/lti/dao/AdminDao.java
+++ Gneral-Insurance-Java/src/main/java/com/lti/dao/AdminDao.java
@@ -1,5 +1,43 @@
 package com.lti.dao;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import com.lti.model.Admin;
+import com.lti.model.User;
 
 public class AdminDao {
+	EntityManagerFactory emf;
+	EntityManager em;
+	EntityTransaction tx;
 
+	  public AdminDao()  {
+		emf = Persistence.createEntityManagerFactory("pu");
+		em = emf.createEntityManager();
+		tx = em.getTransaction();
+	}
+	public void addAAdmin(Admin admin) {
+		tx.begin();
+		em.merge(admin);
+		tx.commit();
+		System.out.println("new admin added.");
+	}
+	public boolean loginAdmin(long adminId,String password) {
+		String jpql="select a from Admin a where a.adminId=:uid and a.password=:psw";
+		Query query=em.createQuery(jpql,User.class);
+		query.setParameter("uid",adminId);
+		query.setParameter("psw",password);
+		if(query.getFirstResult()>0)
+	    {
+	        return true;
+	    }
+	    else
+	    {
+	        return false;
+	    }
+	}
+	public Admin findAdminById(int adminId) {
+		return em.find(Admin.class, adminId);
+	}
 }
--- Gneral-Insurance-Java/src/main/java/com/lti/dao/UserDao.java
+++ Gneral-Insurance-Java/src/main/java/com/lti/dao/UserDao.java
@@ -1,5 +1,63 @@
 package com.lti.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import com.lti.model.Address;
+import com.lti.model.BuyInsurance;
+import com.lti.model.PolicyCategory;
+import com.lti.model.Status;
+import com.lti.model.User;
+import com.lti.model.Vehicle;
+
 public class UserDao {
+	public void addAUser(User user) {
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("pu");  
+		EntityManager em=emf.createEntityManager(); 
+		EntityTransaction tx=em.getTransaction();
+		tx.begin();
+		em.merge(user);
+		tx.commit();
+		System.out.println("user registered.");
+	}
+	public User findAUserByuserId(long userId) {
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("pu"); 
+		EntityManager em=emf.createEntityManager(); 
+		EntityTransaction tx=em.getTransaction();
+		return em.find(User.class, userId);	
+	}
+	
+	public User loginUser(long userId,String pass) {
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("pu");  
+		EntityManager em=emf.createEntityManager(); 
+		EntityTransaction tx=em.getTransaction();
+		String jpql="select u from User u where u.userId=:uid and u.password=:psw";
+		Query query=em.createQuery(jpql,User.class);
+		query.setParameter("uid",userId);
+		query.setParameter("psw",pass);
+		User user=(User)query.getResultList().stream().findFirst().orElse(null);
+		if(user!=null)
+			return user;
+		return null;
+	}
+	public void addAUserWithAAddress(User user) {
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("pu"); 
+		EntityManager em=emf.createEntityManager(); 
+		EntityTransaction tx=em.getTransaction();
+		tx.begin();
+		em.merge(user);
+		tx.commit();
+	}
+	public void buyInsuranceforAVehicle(Vehicle vehicle) {
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("pu"); 
+		EntityManager em=emf.createEntityManager(); 
+		EntityTransaction tx=em.getTransaction();
+		tx.begin();
+		em.merge(vehicle);
+		tx.commit();
+	}
 
 }
